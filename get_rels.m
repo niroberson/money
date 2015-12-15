161b373a8b5a1d8228969d3c924b927b96a16bed
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-14 22:37:56 -0500

    Adding comments

--- get_rels.m
+++ get_rels.m
@@ -1,6 +1,6 @@
 function [ object_rels, subject_rels ] = get_rels( search_cui, object_cui, subject_cui )
-%get_rels Return the primary relationships for a node
-%   Get the primary nodesfunction get_rels(CUI)
+% Get_rels Return the primary relationships for a node
+% Get the primary nodesfunction get_rels(CUI)
 % Get the object/subject concepts from subject/object rels
 object_rels = object_cui(strcmp(subject_cui, search_cui));
 subject_rels = subject_cui(strcmp(object_cui, search_cui));
--- main.m
+++ main.m
@@ -1,17 +1,26 @@
-%% Load data and extract
-% Read in the nodes and relationships files
 clear, clc
+% Read in the nodes and relationships files
 nodes = readtable('concept.csv');
 rels = readtable('Predication_aggregate2.csv');
+
+%% Load data and extract
+subject_column = 6
 subject_cui = table2array(rels(:, 6));
+
+object_column = 10;
 object_cui = table2array(rels(:, 10));
+
 concepts = table2array(nodes(:, [2, 4]));
 
 %% Get node of interest information
-% Search for a concept in the nodes table
+% Get a random subject i.e. #100
 cui_of_interest = subject_cui{100};
+
+% Extract the name of this concept
 concept_of_interest = concepts(strcmp(concepts(:,1), cui_of_interest), 2);
 fprintf('CUI: %s, Concept of Interest: %s\n', cui_of_interest, concept_of_interest{:})
+
+% Get the relationships that describe this concept
 [object_rels, subject_rels] = get_rels(cui_of_interest, object_cui, subject_cui);
 
 % Get unique primary nodes
@@ -28,7 +37,11 @@ for j=1:N_i
     N_i_j = sum(strcmp(subject_rels,cui_j)) + sum(strcmp(object_rels, cui_j)); % Number of relationships between i and j
     [object_rels_j, subject_rels_j] = get_rels(cui_j, object_cui, subject_cui);
     N_j = numel(object_rels_j) + numel(subject_rels_j);
+    
+    % Calculate KSP
     KSP = N_i_j/(N_i+N_j-N_i_j);
+    
+    % Calculate distance of each node from KSP
     d(j) = 1/KSP - 1;
 end
 
