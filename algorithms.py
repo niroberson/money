79bb4cc61b97a11d29f925d2ff6b9d67fa9710d8
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-22 13:54:29 -0500

    Adding print statement

--- algorithms.py
+++ algorithms.py
@@ -27,6 +27,7 @@ class Algorithm:
         direct_nodes = self.query.get_direct_nodes(node_of_interest)
         d = []
         for node in direct_nodes:
+            print('Calculating distance for %s' % node[0].properties['NAME'])
             N_j = self.get_num_rels(node)
             N_i_j = self.get_num_rel(node_of_interest, node)
             KSP = N_i_j/(N_i+N_j-N_i_j)
