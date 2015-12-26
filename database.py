15e1fa1e3d7a96abd7ca808c8c5995436ed566ac
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-26 00:35:24 -0500

    BFS working

--- database.py
+++ database.py
@@ -20,7 +20,7 @@ class Database:
 
     def execute_query(self, cypher_query):
         if self.dev_flag:
-            cypher_query += " LIMIT 5"
+            cypher_query += " LIMIT 15"
         return self.connection.cypher.execute(cypher_query)
 
     def get_node_by_name(self, search_input):
@@ -54,12 +54,27 @@ class Database:
 
     def get_count_direct_edges(self, node):
         cypher_query = "MATCH (a)-[r]-(b) WHERE id(a)=" + str(node.id) + " RETURN count(r)"
-        return self.connection.cypher.execute(cypher_query)
+        count = self.connection.cypher.execute(cypher_query)
+        return count.one
 
     def get_count_direct_nodes(self, node):
         cypher_query = "MATCH (a)-[r]-(b) WHERE id(a)=" + str(node.id) + " RETURN count(b)"
-        return self.connection.cypher.execute(cypher_query)
+        count = self.connection.cypher.execute(cypher_query)
+        return count.one
 
     def get_count_between_nodes(self, node1, node2):
-        cypher_query = "MATCH (a)-[r]-(b) WHERE id(a)=" + node1.id + " AND id(b)=" + node2.id + " RETURN count(r)"
-        return self.connection.cypher.execute(cypher_query)
+        cypher_query = "MATCH (a)-[r]-(b) WHERE id(a)=" + str(node1.id) + " AND id(b)=" + str(node2.id) + " RETURN count(r)"
+        count = self.connection.cypher.execute(cypher_query)
+        return count.one
+
+    def bfs_from_node(self, node, max_level):
+        cypher_query = "MATCH (a)-[r*1.." + str(max_level) + "]-(b) RETURN r"
+        # Create a Node object for every .b and a Edge for every .r
+        response = self.execute_query(cypher_query)
+        edge_list = []
+        node_list = []
+        for edgeX in response:
+            edge_list.append(Edge(edgeX.r[0]))
+            node_list.append(Node(edgeX.r[0].start_node))
+            node_list.append(Node(edgeX.r[0].end_node))
+        return node_list, edge_list
--- factory.py
+++ factory.py
@@ -14,28 +14,34 @@ class RecommenderFactory:
         node_of_interest = self.database.get_node_by_name(concept)
 
         # Get the sub-graph connected to this node
-        nodes = self.database.get_direct_nodes(node_of_interest)
-        edges = self.database.get_direct_edges(node_of_interest)
+
+        # Get BFS level 1
+        nodes, edges = self.database.bfs_from_node(node_of_interest, 2)
+        # nodes = self.database.get_direct_nodes(node_of_interest)
+        # edges = self.database.get_direct_edges(node_of_interest)
+
+        # Get BFS level 2
+
 
         # Compute distances between all concept relationships
         for edgeX in edges:
-            if edgeX.properties.score:
+            if hasattr(edgeX.properties, 'score'):
                 edgeX.distance = edgeX.properties.score
             else:
                 edgeX.distance = self.compute_distance(edgeX.source_node, edgeX.target_node)
-        
+
         # Create a networkX graph based on this node
         self.graph = Graph(nodes, edges)
         # Get a dictionary of the shortest paths based on this node
-        self.graph.get_shortest_paths(node_of_interest)
+        paths = self.graph.get_shortest_paths(node_of_interest)
 
         # Return a table sorted by shortest path lengths
+        print(paths)
 
     def compute_distance(self, node_source, node_target):
-        print('Calculating distance between %s and %s' % (node_source.properties['NAME'], node_target[0].properties['NAME']))
         n_i = self.database.get_count_direct_edges(node_source)
         n_j = self.database.get_count_direct_edges(node_target)
         n_i_j = self.database.get_count_between_nodes(node_source, node_target)
         ksp = n_i_j/(n_i+n_j-n_i_j)
-        d = ([1/ksp - 1])
+        d = 1/ksp - 1
         return d
