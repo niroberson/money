10f7159f51295ed49f4f491b1499ec7efc473f61
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-27 17:38:48 -0500

    Fixing to list

--- results.py
+++ results.py
@@ -4,7 +4,7 @@ from pandas import DataFrame
 class Results:
     def __init__(self, names, paths):
         # Get a dictionary of the shortest paths based on this node
-        distances = paths.values()
+        distances = list(paths.values())
         # Return a table sorted by shortest path lengths
         self.df = DataFrame([names, distances], columns=['Concept', 'distance'])
 
