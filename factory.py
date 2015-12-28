af3ef20f574016cf418e662b7fa980cd6ea8dc7e
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-28 14:24:29 -0500

    Ensuring order is correct

--- factory.py
+++ factory.py
@@ -9,18 +9,22 @@ class RecommenderFactory:
         self.graph = Graph(dev_flag)
 
     def search_concept(self, concept):
+        # Get the node for this concept
         concept_node = self.graph.get_node_by_name(concept)
+
+        # Create the subgraph around this concept node
         self.graph.create_subgraph(concept_node)
 
+        # Find the shortest paths in this subgraph
         paths, path_lengths = self.graph.get_shortest_paths(concept_node)
+
+        # Create results
         path_nodes = path_lengths.keys()
         names = [self.graph.node[nodeX]['properties']['NAME'] for nodeX in path_nodes]
-        # Return a table sorted by shortest path lengths
-        data = {'Concept': names,
-                'Distance': list(path_lengths.values()), 'Path': list(paths.values())}
-        df = DataFrame(data, index=path_nodes)
-        df = df.sort('Distance')
-        results = Results(df)
+        path_names = [paths[nodeX] for nodeX in path_nodes]
+        paths_lengths_l = [path_lengths[nodeX] for nodeX in path_nodes]
+        results = Results(path_nodes, names, paths_lengths_l, path_names)
+
         error = None
         return results, error
 
--- results.py
+++ results.py
@@ -2,8 +2,11 @@ from pandas import DataFrame
 
 
 class Results:
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, path_nodes, names, path_lengths, path_names):
+        data = {'Concept': names,
+                'Distance': path_lengths, 'Path': path_names}
+        df = DataFrame(data, index=path_nodes)
+        self.df = df.sort('Distance')
 
     def to_html(self):
         # Create table from distance results
