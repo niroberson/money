2ad5c2541dc9d12760b673fa419b7f17d1eaaca3
Nathan Roberson <nathaniroberson@gmail.com>
2016-01-06 12:59:02 -0500

    Adding autojit

--- factory.py
+++ factory.py
@@ -1,6 +1,5 @@
 from graph import Graph
 from results import Results
-from numba import autojit
 
 
 class RecommenderFactory(object):
@@ -34,13 +33,20 @@ class RecommenderFactory(object):
         concept_node = self.graph.get_node_by_name(object)
         object_node = self.graph.get_node_by_name(object)
 
-    @autojit
     def traverse_edges(self):
         edge_ids = self.graph.database.get_all_edge_ids()
+        count = 0
         for eid in edge_ids:
+            count += 1
             self.graph.create_edge(eid[0])
+            if count % 1000 == 0:
+                print('On edge %i of %i' % (count, len(edge_ids)))
 
     def traverse_nodes(self):
         node_ids = self.graph.database.get_all_node_ids()
+        count = 0
         for nid in node_ids:
+            count += 1
             self.graph.create_node(nid[0])
+            if count % 1000 == 0:
+                print('On Node %i of %i' % (count, len(node_ids)))
