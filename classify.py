c0c462a394f56205ea29fdb5ddaad4630b402ff9
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-02 03:30:25 -0400

    Adding clustering plots

--- classify.py
+++ classify.py
@@ -3,12 +3,13 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-
+from config import Config
+from database import Database
 
 class Classify(object):
 
-    def __init__(self, database):
-        self.database = database
+    def __init__(self):
+        self.database = Database(Config(False))
         self.features = DataFrame.from_csv('features.csv')
 
     def cluster(self):
@@ -17,7 +18,8 @@ class Classify(object):
         fig = plt.figure(1, figsize=(4, 3))
         ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
         labels = estimator.labels_
-        ax.scatter(self.features[:, 3], self.features[:, 0], self.features[:, 2], c=labels.astype(np.float))
+        ax.scatter(self.features['2'], self.features['2'], self.features['1'], c=labels.astype(np.float))
+        plt.show()
 
 if __name__ == "__main__":
     c = Classify()
