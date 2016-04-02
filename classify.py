749c092a3c7be998ab496258ee5c2d438c2ceb43
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-02 03:18:31 -0400

    Cluster algorithm

--- classify.py
+++ classify.py
@@ -1,10 +1,24 @@
 from pandas import DataFrame
+from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Classify(object):
 
     def __init__(self, database):
         self.database = database
-        features = DataFrame.from_csv('features.csv')
+        self.features = DataFrame.from_csv('features.csv')
 
+    def cluster(self):
+        estimator = KMeans(n_clusters=2)
+        estimator.fit(self.features)
+        fig = plt.figure(1, figsize=(4, 3))
+        ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+        labels = estimator.labels_
+        ax.scatter(self.features[:, 3], self.features[:, 0], self.features[:, 2], c=labels.astype(np.float))
 
+if __name__ == "__main__":
+    c = Classify()
+    c.cluster()
